7f2768b6ca1ca402a49d67e0a9c30b563f0f4fd5
unknown <alicelee.0418@gmail.com>
2022-01-25 17:30:08 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoList from './TodoList.js';
 import AddToDoForm from './AddTodoForm.js';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 
 
 
 function App() {
+  
     
   const [todoList, setTodoList] = React.useState([]);
   const [isLoading, setIsloading] = React.useState(true);
@@ -39,18 +41,25 @@ function App() {
   };
 
   return (
-    
-      <>
-        
-        
-        <h1>Todo List </h1>
-          <AddToDoForm onAddTodo={addTodo} />
-          {(isLoading)? <p>Loading...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>}
-  
-        
-      </>
-   
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element={
+          <>
+           <h1>Todo List</h1>
+           <AddToDoForm onAddTodo={addTodo}/> 
+           {(isLoading)? <p>Loading...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>}
+          </>
+        }/>
+      
+        <Route path = "/new" element={
+          <h3>New Todo List</h3>
+        }/>
+     
+      </Routes>
+    </BrowserRouter> 
   ); 
+       
 }
+
 
 export default App;
